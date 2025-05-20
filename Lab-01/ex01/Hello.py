b0563a47f0ac2815d38a3d26493dca5f2ee1eebf
lengthanhdat <tdat2202a@gmail.com>
2025-05-20 10:46:36 +0700

    update ex04

--- Lab-01/ex01/Hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello,World!")
-print("My name is Dat")
-print("HUTECH University")
--- Lab-01/ex02/ex02_01.py
+++
@@ -1,5 +0,0 @@
-#Nhap ten va tuoi cua nguoi dung
-ten = input("Nhap ten cua ban: ")
-tuoi = input("Nhap tuoi cua ban: ")
-#In thong diep chao mung voi thong tin vua nhap
-print("Chao mung,",ten, "Ban", tuoi, "tuoi.")
